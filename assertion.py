e716fd35ff50a4c928666aea89fb1bbb8c367f66
Carolyn Anderson <canders277@gmail.com>
2018-03-14 10:24:39 -0400

    adding context-sensitivity

--- assertion.py
+++ assertion.py
@@ -62,53 +62,56 @@ def initWPriors(wList):
 	return wProbs
 
 def initCPriors(eList):
-	return urn(eList)
+	#return urn(eList)
+	return [0.4,0.4,0.1,0.1]
 
 def listener0(w,m,wProbs,wVecs):
 	posterior = wVecs[w][m] * wProbs[w]
 	return posterior
 
-def speaker0(o,wProbs,wVecs,cProbs,pList):
+def speaker0(o,wProbs,wVecs,cProbs,mList,eList,pDict,pList):
 	#argmax listener0(w|m) - cost(m)
 	print "Observation", o
 	mProbs = []
-	for p in range(0,len(pList)):
-		print "Considering ",pList[p]
+	for m in range(0,len(mList)):
+		print "Considering ",mList[m]
 		mProb = 0.0
 		for w in o:
-			mProb += listener0(w,p,wProbs,wVecs)
+			for e in range(0,len(eList)):
+				prop = getPerspective(e,m,pDict,mList,eList)
+				mProb += listener0(w,prop,wProbs,wVecs)
 		mProbs.append(mProb)
 	best = 0
 	for p in range(0,len(mProbs)):
 		if mProbs[p] > mProbs[best]:
 			best = p
-	print "Chosen message is: ",pList[p]
-	return p	
+	print mProbs
+	print "Chosen message is: ",pList[best]
+	return best
 
-def getPerspective(e,m,pDict,wList,eList):
-	sub = re.sub("X", eList[e], m)
+def getPerspective(e,m,pDict,mList,eList):
+	sub = re.sub("X", eList[e], mList[m])
 	subdex = pDict[sub]
 	return subdex
 
-def updateWorld(w,m,eList,wProbs,wVecs,cProbs,pDict,pList):
+def updateWorld(w,m,eList,wProbs,wVecs,cProbs,pDict,mList):
 	posterior = 0.0
 	for e in range(0,len(eList)):
-		prop = getPerspective(e,m,pDict,pList,eList)
-		posterior += cProbs[e]*lzero(w,prop,wProbs,wVecs)
+		prop = getPerspective(e,m,pDict,mList,eList)
+		posterior += cProbs[e]*listener0(w,prop,wProbs,wVecs)
 	return posterior
 
-def updatePerspective(e,m,cProbs,wList,eList,wProbs,wVecs,pDict,pList):
+def updatePerspective(e,m,cProbs,wList,eList,wProbs,wVecs,pDict,mList):
 	postSum = 0.0
-	prop = getPerspective(e,m,pDict,pList,eList)
+	prop = getPerspective(e,m,pDict,mList,eList)
 	for w in range(0,len(wList)):
-		postSum += lzero(w,prop,wProbs,wVecs)
+		postSum += listener0(w,prop,wProbs,wVecs)
 	posterior = postSum*cProbs[e]
 	return posterior
 
-def makeCS(wList):
+def makeCS(indexes):
 	cs = []
-	indexes = range(0,len(wList))
-	for i in range(0,len(wList)):
+	for i in range(0,len(indexes)):
 		cs += list(itertools.combinations(indexes,i+1))
 	cs = [list(x) for x in cs]
 	return cs
@@ -120,34 +123,53 @@ def makeObservation(cs):
 def updateCS(wProbs):
 	currW = []
 	for w in range(0,len(wProbs)):
-		if wProbs[w] > 0:
+		if wProbs[w] > 0.0:
 			currW.append(w)
 	return makeCS(currW)
 
 def initPriors(wList,eList):
 	wProbs = initWPriors(wList)
 	cProbs = initCPriors(eList)
-	cs = makeCS(wList)
+	cs = makeCS(range(0,len(wList)))
 	return wProbs,cProbs,cs
 
-def utter(eList,eDict,pList,pDict,wList,wDict,wVecs,wProbs,cProbs,cs):
+def normProbs(probs):
+	norm = sum(probs)
+	return [(p/norm) for p in probs]
+
+def utter(eList,eDict,pList,pDict,wList,wDict,wVecs,wProbs,cProbs,cs,mList):
 	observation = makeObservation(cs)
-	message = speaker0(observation,wProbs,wVecs,cProbs,pList)
+	message = speaker0(observation,wProbs,wVecs,cProbs,mList,eList,pDict,pList)
 	for w in range(0,len(wList)):
 		wProbs[w] = updateWorld(w,message,eList,wProbs,wVecs,cProbs,pDict,pList)
 	for e in range(0,len(eList)):
 		cProbs[e] = updatePerspective(e,message,cProbs,wList,eList,wProbs,wVecs,pDict,pList)
 	cs = updateCS(wProbs)
-	return wProbs,cProbs,cs
+	return normProbs(wProbs),normProbs(cProbs),cs
+
+def evaluate(wProbs,cProbs,cs, label):
+	print label+" world probs: ", wProbs
+	print label+" perspective probs: ", cProbs
+	print label+" size of context set: ", len(cs)
+	return
 
 def main():
+	if len(sys.argv) < 4:
+		print "Usage: python assertion.py worlds entities propositions messages"
+		quit()
 	wfile = sys.argv[1]
 	efile = sys.argv[2]
 	pfile = sys.argv[3]
+	mfile = sys.argv[4]
 	eList,eDict = parseEntities(efile)
 	pList,pDict = parsePropositions(pfile)
 	wList,wDict,wVecs = parseWorlds(wfile)
+	mList, mDict = parsePropositions(mfile)
 	wProbs,cProbs,cs = initPriors(wList,eList)
-	wProbs,cProbs,cs = utter(eList,eDict,pList,pDict,wList,wDict,wProbs,cProbs,cs)
+	evaluate(wProbs,cProbs,cs, "Initial")
+	wProbs,cProbs,cs = utter(eList,eDict,pList,pDict,wList,wDict,wVecs,wProbs,cProbs,cs,mList)
+	evaluate(wProbs,cProbs,cs, "New")
+	wProbs,cProbs,cs = utter(eList,eDict,pList,pDict,wList,wDict,wVecs,wProbs,cProbs,cs,mList)
+	evaluate(wProbs,cProbs,cs, "New")
 main()
 
