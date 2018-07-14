be3507041393ef0892374f98e0c3da7b0a20d32a
Carolyn Anderson <canders277@gmail.com>
2018-07-14 14:47:33 -0400

    format script
    Script for formatting output of webppl program into a csv file for R
    analysis

--- format.py
+++ format.py
@@ -24,39 +24,62 @@ def sep_data(ddict):
 	for s in sents:
 		probDict[s] = {}
 		assert len(ddict[s]) == 1
-		print s
 		data = ddict[s][0].split(',\"support\":')
 		probs = data[0].replace("{\"probs\":","").strip("[]").split(',')
 		support = data[1]
-		support = support.replace(']]} : 1','').strip('[').split('],[')
+		support = support.replace(']]} : 1','').replace('true','True').replace('false','False').strip('[').split('],[')
 		for i in range(0,len(support)):
 			w = support[i]
 			p = probs[i]
 			if w not in worldNames:
 				worldNames[w] = len(worldNames.keys())
 			pdict = probDict[s]
-			pdict[w] = p
+			suptup = eval(w)
+			wdict = suptup[0]
+			wdict["holder"] = suptup[1]['holder']
+			wdict["prob"] = p
+			pdict[w] = wdict
         	probDict[s] = pdict
 	for s in sents:
 		for w in worldNames.keys():
 			if w not in probDict[s]:
 				pdict = probDict[s]
-				pdict[w] = 0
+				suptup = eval(w)
+				wdict = suptup[0]
+				wdict["holder"] = suptup[1]['holder']
+				wdict["prob"] = 0
+				pdict[w] = wdict
 				probDict[s] = pdict
-		for w in probDict[s].keys():
-			print s
-			print w
-			print probDict[s][w]
+	return probDict
 
-
-
+def output_data(pDict,go,cost,dirichlet,pp,filename):
+	outputfile = filename.replace('txt','csv')
+	lineEnd = [go,cost,dirichlet,pp]
+	lines = []
+	for s in pDict.keys():
+		for w in pDict[s].keys():
+			lineList = [s]
+			for entry in pDict[s][w]:
+				lineList.append(str(pDict[s][w][entry]))
+			lineList+= lineEnd
+			line = ','.join(lineList)
+			lines.append(line+'\n')
+	with open(outputfile,'w') as of:
+		for line in lines:
+			of.write(line)
+			
 def main():
-	if len(sys.argv) < 2:
-  		print "Usage: python format.py filename"
+	if len(sys.argv) < 6:
+  		print "Usage: python format.py filename go_is_perspective cost dirichlet pprior"
 	else:
+		go = sys.argv[2]
+		cost = sys.argv[3]
+		dirichlet = sys.argv[4]
+		pp = sys.argv[5]
   		data = open(sys.argv[1],'r').readlines()
   		data = data[0:len(data)-1]
-  		probDict = get_data(data)
-  		sep_data(probDict)
+		dDict = get_data(data)
+  		probDict = sep_data(dDict)
+  		output_data(probDict,go,cost,dirichlet,pp,sys.argv[1])
 
 main()
