02fc820e56d46ebecd63ac76e94730480d4776a7
Carolyn Anderson <canders277@gmail.com>
2018-07-18 20:08:58 -0400

    dirichlet output

--- format_all.sh
+++ format_all.sh
@@ -9,3 +9,15 @@ python format.py gop=0_cost=0.25_p=dir0.txt 0 0.25 0 0
 python format.py gop=0_cost=0.5_p=dir0.txt 0 0.5 0 0
 python format.py gop=0_cost=0.75_p=dir0.txt 0 0.75 0 0
 python format.py gop=0_cost=1_p=dir0.txt 0 1 0 0
+
+python format.py gop=1_cost=0_p=dir1.txt 1 0 1 1
+python format.py gop=1_cost=0.25_p=dir1.txt 1 0.25 1 1
+python format.py gop=1_cost=0.5_p=dir1.txt 1 0.5 1 1
+python format.py gop=1_cost=0.75_p=dir1.txt 1 0.75 1 1
+python format.py gop=1_cost=1_p=dir1.txt 1 1 1 1
+
+python format.py gop=0_cost=0_p=dir1.txt 0 0 1 1
+python format.py gop=0_cost=0.25_p=dir1.txt 0 0.25 1 1
+python format.py gop=0_cost=0.5_p=dir1.txt 0 0.5 1 1
+python format.py gop=0_cost=0.75_p=dir1.txt 0 0.75 1 1
+python format.py gop=0_cost=1_p=dir1.txt 0 1 1 1
