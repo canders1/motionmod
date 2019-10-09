c88291c40baa44e8cdd34e8008df4b5acb5e43f2
Carolyn Anderson <canders277@gmail.com>
2019-10-09 14:49:19 -0400

    speaker

--- run_probgop.sh
+++ run_probgop.sh
@@ -1,4 +1,6 @@
 for cost in 0.0 0.25 0.5 0.75 1
 do
-webppl xnew_prob_aug.wppl $cost > "prob_$cost.txt"
+webppl sample_p_1st.wppl $cost "prob" > "aug_prob/prob_$cost.txt"
 done
+cat aug_prob/prob_*.txt > aug_prob/aug_prob.txt
+python parse_results.py aug_prob/aug_prob.txt prob aug_prob.csv
