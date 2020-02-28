d2ebbf04b55a2ecc37dd240fd84e11e07843f731
canders1 <canders277@gmail.com>
2020-02-28 10:10:27 -0500

    deleted

--- combine.sh
+++
@@ -1,6 +0,0 @@
-rm all.csv
-rm all_dir.csv
-rm all_uni.csv
-cat go*.csv >> all.csv
-cat go*dir*1.csv >> all_dir.csv
-cat go*dir0.csv >> all_uni.csv
--- new_combine.sh
+++
@@ -1,2 +0,0 @@
-rm new_all.csv
-cat new*.csv >> new_all.csv
